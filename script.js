7bf5e7879c82fb0c62cbb0991180de7de2cc8301
jpmunich <parkercoyle8@gmail.com>
2022-07-19 13:26:30 -0400

    All buttons now functional

--- script.js
+++ script.js
@@ -2,54 +2,66 @@ const rockButton = document.getElementById("rock-button");
 const paperButton = document.getElementById("paper-button");
 const scissorsButton = document.getElementById("scissors-button");
 
+function createTheElement(text) {
+    const newP = document.createElement("p");
+    newP.innerText = text;
+    document.body.appendChild(newP);
+    newP.setAttribute("class", "newP")
+}
+
 function playGame(playerSelection, computerSelection) {
     if (playerSelection === "rock") {
         if (computerSelection === 0) {
-            const newP = document.createElement("p");
-            newP.innerText = "Tie!";
-            document.body.appendChild(newP);
+            createTheElement("Tie!");
         }
         else if (computerSelection === 1) {
-            return "Computer Wins!"
+            createTheElement("Computer Wins!");
         }
-        else if (computerSelection === 2) {
-            return "Player Wins!"
+        else if (computerSelection === 2) { 
+            createTheElement("Player Wins!");
         }
         else {
-            return "ERROR!"
+            alert("ERROR!");
         }
     }
     else if (playerSelection === "paper") {
         if (computerSelection === 0) {
-            return "Player Wins!"
+            createTheElement("Player Wins!");
         }
         else if (computerSelection === 1) {
-            return "Tie!"
+            createTheElement("Tie!");
         }
         else if (computerSelection === 2) {
-            return "Computer Wins!"
+            createTheElement("Computer Wins!");
         }
         else {
-            return "ERROR!"
+            alert("ERROR!");
         }
     }
     else if (playerSelection === "scissors") {
         if (computerSelection === 0) {
-            return "Computer Wins!"
+            createTheElement("Computer Wins!");
         }
         else if (computerSelection === 1) {
-            return "Player Wins!"
+            createTheElement("Player Wins!");
         }
         else if (computerSelection === 2) {
-            return "Tie!"
+            createTheElement("Tie!");
         }
         else {
-            return "ERROR!"
+            alert("ERROR!");
         }
     }
 }
 
 rockButton.addEventListener("click", function() {
-    const select = computerSelect();
+    playGame("rock", Math.floor(Math.random() * 3));
+});
+
+paperButton.addEventListener("click", function() {
+    playGame("rock", Math.floor(Math.random() * 3));
+});
+
+scissorsButton.addEventListener("click", function() {
     playGame("rock", Math.floor(Math.random() * 3));
 });
